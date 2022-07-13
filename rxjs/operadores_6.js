fbd6cf2e97220d5b71d89e57c0dbfb786b1880c5
uhzero <aureo.moraes@gmail.com>
2022-07-13 14:09:34 -0300

    newPipeOp

--- rxjs/operadores_6.js
+++ rxjs/operadores_6.js
@@ -1,24 +1,41 @@
 const { from, Observable } = require('rxjs')
 
-function newpipeOp (nextFn) {
+function newpipeOp (opFn) {
     return function (source) {
         return Observable.create(subscriber => {
+            const sub = opFn(subscriber)
             source.subscribe({
-                next(value){
-                    nextFn(subscriber, value)
-                }
+                next: sub.next,
+                error: sub.error || (e => subscriber.error(e)),
+                complete: sub.complete || (() => subscriber.complete())
             })
         })
     }
 }
 
 function firstEl () {
-    return newpipeOp((subscriber, value) => {
-        subscriber.next(value)
-        subscriber.complete()
-    })
+    return newpipeOp(subscriber => ({
+        next(value){
+            subscriber.next(value)
+            subscriber.complete()
+        }
+    }))
+}
+
+function lastEl () {
+    let el
+    return newpipeOp(subscriber => ({
+        next(value){
+            el = value
+        },
+        complete(){
+            subscriber.next(el)
+            subscriber.complete()
+        }
+
+    }))
 }
 
 from([1, 2, 3, 4])
-    .pipe(firstEl())
+    .pipe(lastEl())
     .subscribe(console.log)
